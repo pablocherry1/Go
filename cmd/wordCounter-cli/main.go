d22a0eb5f9377c77f0f97e9db036f13e840168a2
Hodgins Ohannigan Dell <gjuanpablo746@gmail.com>
2025-08-13 18:07:39 -0500

    bufio

--- cmd/wordCounter-cli/main.go
+++ cmd/wordCounter-cli/main.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
-	"errors"
+	"os"
+	"bufio"
 )
 
 func main() {
-	
-	myError := errors.New("Something went wrong in the funtion")
-	fmt.Println(myError)
+
+	fileName := "not-a-real-file.txt"
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		myError := fmt.Errorf("Could not open file '%s': %w", fileName, err)
+		fmt.Println(myError)
+		return
+	} else {
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		scanner.Split(bufio.ScanWords)
+
+		wordCount := 0
+
+		for scanner.Scan() {
+			wordCount++
+		}
+
+		fmt.Printf("The file has %d words. \n", wordCount)
+	}
 }
