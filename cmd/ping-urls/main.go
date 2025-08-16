97ed4aca029ad6cd5dae649a5442f9fd5a21a22f
Hodgins Ohannigan <gjuanpablo746@gmail.com>
2025-08-16 07:25:38 -0500

    WaitGroup

--- cmd/ping-urls/main.go
+++ cmd/ping-urls/main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func sayHello() {
+func sayHello(wg *sync.WaitGroup) {
+	defer wg.Done() //This tells the WaitGroup this goroutine is done
 	time.Sleep(1 * time.Second) //Wait for 2 seconds
 	fmt.Println("Hello from the goroutine!")
 }
 
 func main () {
-	go sayHello()
+	var wg sync.WaitGroup
+	wg.Add(1) // Tell the WaitGroup we have 1 goroutine to wait for
+
+
+	go sayHello(&wg)
 	fmt.Println("Hello from the main function!")
-	time.Sleep(2 * time.Second)
+
+	wg.Wait() // Wait for the goroutine to finish
 }
